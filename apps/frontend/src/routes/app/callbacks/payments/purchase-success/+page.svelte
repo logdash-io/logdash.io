0f8f87e8a3e941ef3af59062fbe5900b6e19683c
szymeo <dev.sgracki@gmail.com>
2025-05-20 20:14:41 +0200

    fix: payment success callback page not displaying

--- apps/frontend/src/routes/app/callbacks/payments/purchase-success/+page.svelte
+++ apps/frontend/src/routes/app/callbacks/payments/purchase-success/+page.svelte
@@ -14,7 +14,7 @@
 		start: 1.1,
 		easing: cubicInOut,
 	}}
-	class="bg-base-300 absolute flex min-h-screen w-full items-center justify-center"
+	class="bg-base-300 flex min-h-screen w-full items-center justify-center"
 >
 	<div class="card ld-card-base w-96 rounded-2xl shadow-xl">
 		<div class="card-body items-center gap-4 p-6 text-center">
