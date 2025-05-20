b6ae10567d759e823eb27d9f21742dcce734d452
Aleksander Błaszkiewicz <aleksander.blaszkiewicz@flip.shop>
2025-05-20 21:05:07 +0200

    feat(): wip

--- apps/backend/src/log/events/log-event.emitter.ts
+++ apps/backend/src/log/events/log-event.emitter.ts
@@ -8,6 +8,8 @@ export class LogEventEmitter {
   public constructor(private readonly eventEmitter: EventEmitter) {}
 
   public async emitLogCreatedEvent(payload: LogCreatedEvent): Promise<void> {
+    console.log('Emitting log created event');
+
     this.eventEmitter.emit(LogEvents.LogCreatedEvent, payload);
   }
 }
