d6d29dc5b167c858e2d69911ea60aa95817f8bd7
szymeo <dev.sgracki@gmail.com>
2025-05-31 12:55:33 +0200

    feat: selective route session recording

--- apps/frontend/src/routes/+layout.svelte
+++ apps/frontend/src/routes/+layout.svelte
@@ -34,11 +34,11 @@
 				ui_host: envConfig.posthog.host,
 				person_profiles: 'always',
 				// person_profiles: 'identified_only'
-				disable_session_recording: false,
-				// disable_session_recording: true,
-				// loaded(ph) {
-				// 	loadedPosthogInstance = ph;
-				// },
+				// disable_session_recording: false,
+				disable_session_recording: true,
+				loaded(ph) {
+					loadedPosthogInstance = ph;
+				},
 			});
 			setContext('posthog', posthog);
 		}
@@ -48,29 +48,28 @@
 		logger.debug('Tab ID:', getContext('tabId'));
 	});
 
-	// todo: enable after resolving posthog support #31569
-	// $effect(() => {
-	// 	if (!loadedPosthogInstance) {
-	// 		return;
-	// 	}
+	$effect(() => {
+		if (!loadedPosthogInstance) {
+			return;
+		}
 
-	// 	if (
-	// 		RECORDED_ROUTES.some((path) => page.url.pathname.includes(path)) &&
-	// 		!isDev()
-	// 	) {
-	// 		logger.info(
-	// 			'Starting session recording for route:',
-	// 			page.url.pathname,
-	// 		);
-	// 		loadedPosthogInstance.startSessionRecording();
-	// 	} else {
-	// 		logger.info(
-	// 			'Not starting session recording for route:',
-	// 			page.url.pathname,
-	// 		);
-	// 		loadedPosthogInstance.stopSessionRecording();
-	// 	}
-	// });
+		if (
+			RECORDED_ROUTES.some((path) => page.url.pathname.includes(path)) &&
+			!isDev()
+		) {
+			logger.info(
+				'Starting session recording for route:',
+				page.url.pathname,
+			);
+			loadedPosthogInstance.startSessionRecording();
+		} else {
+			logger.info(
+				'Not starting session recording for route:',
+				page.url.pathname,
+			);
+			loadedPosthogInstance.stopSessionRecording();
+		}
+	});
 
 	$effect(() => {
 		exposedConfigState.set(data.exposedConfig);
