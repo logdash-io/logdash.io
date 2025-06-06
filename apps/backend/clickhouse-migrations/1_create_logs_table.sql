f8f2a6a45ca6c01ce39ae3a39cd2a9990cb9c2c7
Aleksander Błaszkiewicz <aleksander.blaszkiewicz@flip.shop>
2025-06-06 21:31:46 +0200

    feat(): wip

---
+++ apps/backend/clickhouse-migrations/1_create_logs_table.sql
@@ -0,0 +1,26 @@
+CREATE TABLE logs (
+    id UUID DEFAULT generateUUIDv4(),
+    
+    project_id FixedString(24),
+    
+    timestamp DateTime64(3) DEFAULT now64(3),
+    sequence_number UInt32 DEFAULT 0,
+
+    level Enum8(
+        'info' = 1,
+        'warning' = 2,
+        'error' = 3,
+        'silly' = 4,
+        'http' = 5,
+        'debug' = 6,
+        'verbose' = 7,
+    ),
+    
+    message String,
+) ENGINE = MergeTree()
+
+PARTITION BY toYYYYMMDD(timestamp)
+
+ORDER BY (project_id, timestamp, sequence_number)
+
+SETTINGS index_granularity = 8192; 
