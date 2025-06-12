829c46f8e890f9eb960ad3d089929e438dfa745f
b-sw <45360754+b-sw@users.noreply.github.com>
2025-06-12 18:09:48 +0200

    move method

--- apps/backend/src/http-ping-bucket/aggregation/http-ping-bucket-aggregation.service.ts
+++ apps/backend/src/http-ping-bucket/aggregation/http-ping-bucket-aggregation.service.ts
@@ -72,7 +72,7 @@ export class HttpPingBucketAggregationService {
     grouping: BucketGrouping,
     expectedBucketCount: number,
   ): Promise<(HttpPingBucketNormalized | null)[]> {
-    const completeBuckets = HttpPingBucketNormalized.fromExisting(
+    const completeBuckets = this.fillWithEmptyBuckets(
       existingBuckets,
       fromDate,
       grouping,
@@ -84,6 +84,55 @@ export class HttpPingBucketAggregationService {
     }
 
     return completeBuckets;
+  }
+
+  private fillWithEmptyBuckets(
+    existingBuckets: HttpPingBucketNormalized[],
+    fromDate: Date,
+    grouping: BucketGrouping,
+    expectedCount: number,
+  ): (HttpPingBucketNormalized | null)[] {
+    const buckets: (HttpPingBucketNormalized | null)[] = [];
+    const existingBucketsMap = new Map<string, HttpPingBucketNormalized>();
+
+    existingBuckets.forEach((bucket) => {
+      const key = this.getBucketKey(bucket.timestamp, grouping);
+      existingBucketsMap.set(key, bucket);
+    });
+
+    const oneHourMs = 60 * 60 * 1000;
+    const oneDayMs = 24 * oneHourMs;
+    const increment = grouping === BucketGrouping.Hour ? oneHourMs : oneDayMs;
+    let currentDate = new Date(fromDate);
+
+    if (grouping === BucketGrouping.Hour) {
+      currentDate.setMinutes(0, 0, 0);
+    } else {
+      currentDate.setHours(0, 0, 0, 0);
+    }
+
+    for (let i = 0; i < expectedCount; i++) {
+      const bucketKey = this.getBucketKey(currentDate, grouping);
+      const existingBucket = existingBucketsMap.get(bucketKey);
+
+      if (existingBucket) {
+        buckets.push(existingBucket);
+      } else {
+        buckets.push(null);
+      }
+
+      currentDate = new Date(currentDate.getTime() + increment);
+    }
+
+    return buckets.reverse();
+  }
+
+  private getBucketKey(date: Date, grouping: BucketGrouping): string {
+    if (grouping === BucketGrouping.Hour) {
+      return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getHours()}`;
+    } else {
+      return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+    }
   }
 
   private async tryAddVirtualBucket(
--- apps/backend/src/http-ping-bucket/core/entities/http-ping-bucket.interface.ts
+++ apps/backend/src/http-ping-bucket/core/entities/http-ping-bucket.interface.ts
@@ -1,5 +1,3 @@
-import { BucketGrouping } from '../../read/http-ping-bucket-read.service';
-
 export class HttpPingBucketNormalized {
   id: string;
   httpMonitorId: string;
@@ -7,55 +5,6 @@ export class HttpPingBucketNormalized {
   successCount: number;
   failureCount: number;
   averageLatencyMs: number;
-
-  public static fromExisting(
-    existingBuckets: HttpPingBucketNormalized[],
-    fromDate: Date,
-    grouping: BucketGrouping,
-    expectedCount: number,
-  ): (HttpPingBucketNormalized | null)[] {
-    const buckets: (HttpPingBucketNormalized | null)[] = [];
-    const existingBucketsMap = new Map<string, HttpPingBucketNormalized>();
-
-    existingBuckets.forEach((bucket) => {
-      const key = this.getBucketKey(bucket.timestamp, grouping);
-      existingBucketsMap.set(key, bucket);
-    });
-
-    const oneHourMs = 60 * 60 * 1000;
-    const oneDayMs = 24 * oneHourMs;
-    const increment = grouping === BucketGrouping.Hour ? oneHourMs : oneDayMs;
-    let currentDate = new Date(fromDate);
-
-    if (grouping === BucketGrouping.Hour) {
-      currentDate.setMinutes(0, 0, 0);
-    } else {
-      currentDate.setHours(0, 0, 0, 0);
-    }
-
-    for (let i = 0; i < expectedCount; i++) {
-      const bucketKey = this.getBucketKey(currentDate, grouping);
-      const existingBucket = existingBucketsMap.get(bucketKey);
-
-      if (existingBucket) {
-        buckets.push(existingBucket);
-      } else {
-        buckets.push(null);
-      }
-
-      currentDate = new Date(currentDate.getTime() + increment);
-    }
-
-    return buckets.reverse();
-  }
-
-  private static getBucketKey(date: Date, grouping: BucketGrouping): string {
-    if (grouping === BucketGrouping.Hour) {
-      return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getHours()}`;
-    } else {
-      return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
-    }
-  }
 }
 
 export class HttpPingBucketSerialized {
