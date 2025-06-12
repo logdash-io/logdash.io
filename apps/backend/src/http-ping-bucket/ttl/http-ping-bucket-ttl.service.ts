1d16a2693e3da27a56bb9e78a87d3bf5441a246c
b-sw <45360754+b-sw@users.noreply.github.com>
2025-06-12 22:15:06 +0200

    optimise date operations

--- apps/backend/src/http-ping-bucket/ttl/http-ping-bucket-ttl.service.ts
+++ apps/backend/src/http-ping-bucket/ttl/http-ping-bucket-ttl.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { subDays } from 'date-fns';
 import { HttpPingBucketWriteService } from '../../http-ping-bucket/write/http-ping-bucket-write.service';
 
 @Injectable()
@@ -9,9 +10,7 @@ export class HttpPingBucketTtlService {
   @Cron(CronExpression.EVERY_HOUR)
   public async deleteOldBuckets(): Promise<void> {
     const bucketTtlDays = 90;
-    const bucketTtlMs = bucketTtlDays * 24 * 60 * 60 * 1000;
-
-    const cutoffDate = new Date(Date.now() - bucketTtlMs);
+    const cutoffDate = subDays(new Date(), bucketTtlDays);
     await this.httpPingBucketWriteService.deleteOlderThan(cutoffDate);
   }
 }
