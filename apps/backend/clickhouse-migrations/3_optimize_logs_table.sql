3882b3aa8eceaa0a57e9b18f1b8510b1739f8cd0
Aleksander Błaszkiewicz <aleksander.blaszkiewicz@flip.shop>
2025-06-13 11:42:35 +0200

    fix: revert modified migrations

--- apps/backend/clickhouse-migrations/3_optimize_logs_table.sql
+++ apps/backend/clickhouse-migrations/3_optimize_logs_table.sql
@@ -1,9 +1,9 @@
-ALTER TABLE logs
-MODIFY COLUMN id FixedString (24) CODEC (ZSTD),
-MODIFY COLUMN project_id FixedString (24) CODEC (ZSTD),
-MODIFY COLUMN created_at DateTime64 (3) DEFAULT now64 (3) CODEC (Delta, ZSTD),
-MODIFY COLUMN sequence_number UInt32 DEFAULT 0 CODEC (Delta, ZSTD),
-MODIFY COLUMN level Enum8 (
+ALTER TABLE logs 
+MODIFY COLUMN id FixedString(24) CODEC(ZSTD),
+MODIFY COLUMN project_id FixedString(24) CODEC(ZSTD),
+MODIFY COLUMN created_at DateTime64(3) DEFAULT now64(3) CODEC(Delta, ZSTD),
+MODIFY COLUMN sequence_number UInt32 DEFAULT 0 CODEC(Delta, ZSTD),
+MODIFY COLUMN level Enum8(
     'info' = 1,
     'warning' = 2,
     'error' = 3,
@@ -11,7 +11,7 @@ MODIFY COLUMN level Enum8 (
     'http' = 5,
     'debug' = 6,
     'verbose' = 7,
-) CODEC (ZSTD),
-MODIFY COLUMN message String CODEC (ZSTD);
+) CODEC(ZSTD),
+MODIFY COLUMN message String CODEC(ZSTD);
 
-OPTIMIZE TABLE logs FORCE;
+OPTIMIZE TABLE logs FORCE; 
--- apps/backend/clickhouse-migrations/4_optimize_http_pings_table.sql
+++ apps/backend/clickhouse-migrations/4_optimize_http_pings_table.sql
@@ -1,9 +1,9 @@
-ALTER TABLE http_pings
-MODIFY COLUMN id FixedString (24) CODEC (ZSTD),
-MODIFY COLUMN http_monitor_id FixedString (24) CODEC (ZSTD),
-MODIFY COLUMN created_at DateTime64 (3) DEFAULT now64 (3) CODEC (Delta, ZSTD),
-MODIFY COLUMN status_code UInt16 CODEC (ZSTD),
-MODIFY COLUMN response_time_ms UInt32 CODEC (Delta, ZSTD),
-MODIFY COLUMN message Nullable (String) CODEC (ZSTD);
+ALTER TABLE http_pings 
+MODIFY COLUMN id FixedString(24) CODEC(ZSTD),
+MODIFY COLUMN http_monitor_id FixedString(24) CODEC(ZSTD),
+MODIFY COLUMN created_at DateTime64(3) DEFAULT now64(3) CODEC(Delta, ZSTD),
+MODIFY COLUMN status_code UInt16 CODEC(ZSTD),
+MODIFY COLUMN response_time_ms UInt32 CODEC(Delta, ZSTD),
+MODIFY COLUMN message Nullable(String) CODEC(ZSTD);
 
-OPTIMIZE TABLE http_pings FORCE;
+OPTIMIZE TABLE http_pings FORCE; 
