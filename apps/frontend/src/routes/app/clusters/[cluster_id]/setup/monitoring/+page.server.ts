81c33cd02060455bfb5b236e4ebae58ad060e9f5
szymeo <dev.sgracki@gmail.com>
2025-06-24 22:34:43 +0200

    fix: enable monitoring in production

--- apps/frontend/src/routes/app/clusters/[cluster_id]/setup/monitoring/+page.server.ts
+++ apps/frontend/src/routes/app/clusters/[cluster_id]/setup/monitoring/+page.server.ts
@@ -1,38 +1,35 @@
-import { isDev } from '$lib';
 import { logdashAPI } from '$lib/shared/logdash.api';
 import { get_access_token } from '$lib/shared/utils/cookies.utils';
 import { redirect, type ServerLoadEvent } from '@sveltejs/kit';
 
 export const load = async ({
-	cookies,
-	params,
+  cookies,
+  params,
 }: ServerLoadEvent): Promise<{
-	project_id: string;
-	api_key: string;
+  project_id: string;
+  api_key: string;
 }> => {
-	if (!isDev()) {
-		redirect(302, '/app/clusters');
-	}
+  redirect(302, '/app/clusters');
 
-	const clusterId = params.cluster_id;
-	if (!clusterId) {
-		throw new Error('Cluster ID is required');
-	}
-	const projects = await logdashAPI.get_cluster_projects(
-		clusterId,
-		get_access_token(cookies),
-	);
-	if (!projects) {
-		throw new Error('Projects not found');
-	}
-	const { id: projectId } = projects[0];
-	const [project_api_key] = await logdashAPI.get_project_api_keys(
-		get_access_token(cookies),
-		projectId,
-	);
+  const clusterId = params.cluster_id;
+  if (!clusterId) {
+    throw new Error('Cluster ID is required');
+  }
+  const projects = await logdashAPI.get_cluster_projects(
+    clusterId,
+    get_access_token(cookies),
+  );
+  if (!projects) {
+    throw new Error('Projects not found');
+  }
+  const { id: projectId } = projects[0];
+  const [project_api_key] = await logdashAPI.get_project_api_keys(
+    get_access_token(cookies),
+    projectId,
+  );
 
-	return {
-		project_id: projectId,
-		api_key: project_api_key,
-	};
+  return {
+    project_id: projectId,
+    api_key: project_api_key,
+  };
 };
