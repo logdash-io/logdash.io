18263c46f196903a83f0bc99a3bcf6c4679ad2f7
Aleksander Błaszkiewicz <aleksander.blaszkiewicz@flip.shop>
2025-07-07 12:55:17 +0200

    fix: fix project and cluster tier propagation

--- apps/backend/src/project/core/project-core.service.ts
+++ apps/backend/src/project/core/project-core.service.ts
@@ -3,8 +3,7 @@ import { OnEvent } from '@nestjs/event-emitter';
 import { UserEvents } from '../../user/events/user-events.enum';
 import { UserTierChangedEvent } from '../../user/events/definitions/user-tier-changed.event';
 import { ProjectWriteService } from '../write/project-write.service';
-import { UserTier } from '../../user/core/enum/user-tier.enum';
-import { ProjectTier } from './enums/project-tier.enum';
+import { projectTierFromUserTier } from './enums/project-tier.enum';
 
 @Injectable()
 export class ProjectCoreService {
@@ -12,12 +11,7 @@ export class ProjectCoreService {
 
   @OnEvent(UserEvents.UserTierChanged)
   public async updateProjectsTiers(payload: UserTierChangedEvent): Promise<void> {
-    const newProjectTier = {
-      [UserTier.Free]: ProjectTier.Free,
-      [UserTier.EarlyBird]: ProjectTier.EarlyBird,
-      [UserTier.Contributor]: ProjectTier.Contributor,
-      [UserTier.Admin]: ProjectTier.Admin,
-    }[payload.newTier];
+    const newProjectTier = projectTierFromUserTier(payload.newTier);
 
     await this.projectWriteService.updateTiersByCreatorId(payload.userId, newProjectTier);
   }
