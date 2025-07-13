e0b9e8dba30d9f34370669ab028d565761953d83
Aleksander Błaszkiewicz <aleksander.blaszkiewicz@flip.shop>
2025-07-13 13:51:05 +0200

    feat: add logs

--- apps/backend/src/custom-domain/dns/custom-domain-dns.service.ts
+++ apps/backend/src/custom-domain/dns/custom-domain-dns.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { promisify } from 'util';
 import dns from 'dns/promises';
+import { Logger } from '@logdash/js-sdk';
 
 @Injectable()
 export class CustomDomainDnsService {
+  constructor(private readonly logger: Logger) {}
+
   public async checkCnameRecord(domain: string): Promise<string | null> {
     try {
       const results = await dns.resolveCname(domain);
+      this.logger.info('CNAME records', {
+        domain,
+        results,
+      });
       if (results && results.length > 0) {
         return results[0];
       }
