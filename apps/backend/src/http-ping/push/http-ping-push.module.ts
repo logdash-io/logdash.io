9de11feccd517c4045f7c15f017f9eecf2a2e52b
Aleksander Błaszkiewicz <aleksander.blaszkiewicz@flip.shop>
2025-07-13 18:18:32 +0200

    fix: fix DI

--- apps/backend/src/http-ping/push/http-ping-push.module.ts
+++ apps/backend/src/http-ping/push/http-ping-push.module.ts
@@ -3,9 +3,10 @@ import { HttpMonitorReadModule } from '../../http-monitor/read/http-monitor-read
 import { HttpPingWriteModule } from '../write/http-ping-write.module';
 import { HttpPingPingerModule } from '../pinger/http-ping-pinger.module';
 import { HttpPingPushService } from './http-ping-push.service';
+import { ProjectReadModule } from 'src/project/read/project-read.module';
 
 @Module({
-  imports: [HttpMonitorReadModule, HttpPingWriteModule, HttpPingPingerModule],
+  imports: [HttpMonitorReadModule, HttpPingWriteModule, HttpPingPingerModule, ProjectReadModule],
   providers: [HttpPingPushService],
   exports: [HttpPingPushService],
 })
