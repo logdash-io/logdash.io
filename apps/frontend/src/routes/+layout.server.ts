4960fb98ef02defe35784d48a400865a610822f6
Simon <dev.sgracki@gmail.com>
2025-12-16 00:45:44 +0100

    fix: og imgs (#212)
    * feat: wip
    
    * feat: wip
    
    * feat: wip

--- apps/frontend/src/routes/+layout.server.ts
+++ apps/frontend/src/routes/+layout.server.ts
@@ -1,10 +1,13 @@
 import type { ExposedConfig } from '$lib/domains/shared/exposed-config/domain/exposed-config.js';
 import { logdashAPI } from '$lib/domains/shared/logdash.api.server.js';
 
-export const load = async (): Promise<{
+export const load = async ({
+  url,
+}): Promise<{
   exposedConfig: ExposedConfig;
+  origin: string;
 }> => {
   const exposedConfig = await logdashAPI.get_exposed_config();
 
-  return { exposedConfig };
+  return { exposedConfig, origin: url.origin };
 };
